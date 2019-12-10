9d639790d288d66a2b7fc0e9c82010b1e8e410de
dartuchiwa <dartuchiwa@gmail.com>
2019-12-10 22:23:17 +1100

    Feat - start canvas

--- App.tsx
+++ App.tsx
@@ -8,95 +8,192 @@
  * @format
  */
 
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import React, {
+  createContext,
+  useRef,
+  PropsWithChildren,
+  useEffect,
+  useContext,
+  useMemo,
+  useState,
+} from 'react';
+import {View} from 'react-native';
 import {GLView, ExpoWebGLRenderingContext} from 'expo-gl';
 import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
-import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader';
-import {Scene, AmbientLight, LoadingManager} from 'three';
+import {GLTFLoader, GLTF} from 'three/examples/jsm/loaders/GLTFLoader';
+import {
+  LoadingManager,
+  WebGLRenderer,
+  PerspectiveCamera,
+  Object3D,
+  AmbientLight,
+  Camera,
+} from 'three';
 import {Asset} from 'react-native-unimodules';
 import ExpoTextureLoader from './TextureLoader';
+import * as option from 'fp-ts/lib/Option';
+import {pipe} from 'fp-ts/lib/pipeable';
 
-const onContextCreate = (gl: ExpoWebGLRenderingContext) => {
-  let requestId: number;
-  //@ts-ignore
-  const {drawingBufferWidth: width, drawingBufferHeight: height} = gl;
-  const renderer = new THREE.WebGLRenderer({
-    canvas: {
-      width,
-      height,
-      style: {},
-      addEventListener: () => {},
-      removeEventListener: () => {},
-      clientHeight: height,
-    },
-    context: gl,
-  });
-  renderer.setSize(width, height);
-  renderer.setClearColor(0xffffff, 1);
+declare type CanvasContext = {
+  renderer: WebGLRenderer;
+  gl: ExpoWebGLRenderingContext;
+  height: number;
+  width: number;
+  subscribers: Array<() => void>;
+  renders: Array<[() => void, number]>;
+};
+declare type MaybeCanvasContext = CanvasContext | undefined;
 
-  const camera = new THREE.PerspectiveCamera(75, width / height, 1, 1100);
-  /*   const scene = new THREE.Scene(); */
-  let cube: THREE.Mesh;
-  let scene: Scene;
-  console.log(Asset.fromModule(require('./slotscene.glb')).uri);
-  const manager = new LoadingManager();
-  manager.addHandler(/.png/, new ExpoTextureLoader(manager));
-  new GLTFLoader(manager).load(
-    Asset.fromModule(require('./slotscene.gltf')).uri,
-    assets => {
-      scene = new Scene();
-      scene.add(new AmbientLight(0x404040, 2));
-      scene.add(assets.scene);
-      init();
-      animate();
-    },
-  );
-  function init() {
-    camera.position.z = 2;
-    const cube = scene.getObjectByName('SlotMachine');
-    if (cube) {
-      camera.lookAt(cube.position);
-    }
+const CanvasContext = createContext<MaybeCanvasContext>(undefined);
 
-    /*  let geometry = new THREE.BoxGeometry(200, 200, 200);
-    for (let i = 0; i < geometry.faces.length; i += 2) {
-      let hex = Math.random() * 0xffffff;
-      geometry.faces[i].color.setHex(hex);
-      geometry.faces[i + 1].color.setHex(hex);
-    }
-    let material = new THREE.MeshBasicMaterial({
-      vertexColors: THREE.FaceColors,
+const useGLTF = (module: number) => {
+  const gltf = useRef<option.Option<GLTF>>(option.none);
+  const [loaded, setLoaded] = useState(false);
+  const fold = (callback: (glft: GLTF) => void) =>
+    pipe(
+      gltf.current,
+      option.fold(
+        () => {},
+        glft => callback(glft),
+      ),
+    );
+  useEffect(() => {
+    const manager = new LoadingManager();
+    manager.addHandler(/.png/, new ExpoTextureLoader(manager));
+    new GLTFLoader(manager).load(Asset.fromModule(module).uri, assets => {
+      gltf.current = option.some(assets);
+      setLoaded(true);
     });
-    cube = new THREE.Mesh(geometry, material);
-    cube.position.y = 150;
-    scene.add(cube);
-    return cube; */
-  }
-
-  const animate = () => {
-    requestId = requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-    const cube = scene.getObjectByName('Cube');
-    if (cube) {
-      cube.rotation.y += 0.05;
-      cube.rotation.x += 0.02;
-      cube.rotation.z += 0.03;
-    }
-
-    gl.endFrameEXP();
+  }, []);
+  return {
+    fold: fold,
+    isNone: useMemo(() => option.isNone(gltf.current), [
+      option.isNone(gltf.current),
+    ]),
+    foldObjectByName: (name: string) => (
+      callback: (object: Object3D) => void,
+    ) =>
+      pipe(
+        gltf.current,
+        option.map(glft => glft.scene),
+        option.map(scene => option.fromNullable(scene.getObjectByName(name))),
+        option.chain(o => o),
+        option.fold(
+          () => {},
+          o => callback(o),
+        ),
+      ),
   };
 };
-const App = () => {
-  console.log('here');
 
+const useCanvas = (): CanvasContext => {
+  const context = useContext(CanvasContext);
+  if (!context) {
+    throw new Error('Context not loaded');
+  }
+  return {
+    ...context,
+  };
+};
+const useRender = (cb: () => void, priority: number = 1) => {
+  const {renders} = useCanvas();
+
+  useEffect(() => {
+    console.log('in use renderer');
+    const index = renders.push([cb, priority]);
+    return () => {
+      renders.splice(index, 1);
+    };
+  }, []);
+};
+const MainScene = () => {
+  const {height, width, renderer} = useCanvas();
+  const {current: camera} = useRef(
+    new PerspectiveCamera(45, width / height, 1, 1000),
+  );
+  const {fold, isNone, foldObjectByName} = useGLTF(require('./slotscene.gltf'));
+  useEffect(() => {
+    fold(glft => {
+      camera.position.z = 4;
+      glft.scene.add(new AmbientLight(0x404040, 2));
+    });
+    foldObjectByName('SlotMachine')(object => {
+      camera.lookAt(object.position);
+    });
+  }, [isNone]);
+  useRender(() => {
+    fold(glft => {
+      renderer.render(glft.scene, camera);
+    });
+  });
+  return <></>;
+};
+const Canvas = ({children}: PropsWithChildren<{}>) => {
+  const [loaded, setLoaded] = useState(false);
+  const requestId = useRef<number>();
+  const {current: renders} = useRef<Array<[() => void, number]>>([]);
+  const ref = useRef<
+    Pick<CanvasContext, 'gl' | 'height' | 'width' | 'renderer'>
+  >();
+  const renderMain = () => {
+    renders.reduce(
+      ([cb, p], [cb2, p2]) => {
+        return p > p2 ? [cb, p] : [cb2, p2];
+      },
+      [() => {}, 0],
+    )[0]();
+  };
+  const animate = () => {
+    requestId.current = requestAnimationFrame(animate);
+    renderMain();
+    ref.current!.gl.endFrameEXP();
+  };
+  const onContextCreate = (
+    gl: ExpoWebGLRenderingContext & {
+      drawingBufferWidth: number;
+      drawingBufferHeight: number;
+    },
+  ) => {
+    const {drawingBufferWidth: width, drawingBufferHeight: height} = gl;
+    const renderer = new THREE.WebGLRenderer({
+      canvas: {
+        width,
+        height,
+        style: {},
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        clientHeight: height,
+      },
+      context: gl,
+    });
+    renderer.setSize(width, height);
+    renderer.setClearColor(0xffffff, 1);
+    ref.current = {
+      gl,
+      width,
+      height,
+      renderer,
+    };
+    animate();
+    setLoaded(true);
+  };
   return (
-    <View style={{flex: 1}}>
+    <>
       <GLView style={{flex: 1}} onContextCreate={onContextCreate} />
-    </View>
+      {loaded ? (
+        <CanvasContext.Provider
+          value={{...ref.current!, renders, subscribers: []}}>
+          {children}
+        </CanvasContext.Provider>
+      ) : null}
+    </>
+  );
+};
+const App = () => {
+  return (
+    <Canvas>
+      <MainScene />
+    </Canvas>
   );
 };
 
